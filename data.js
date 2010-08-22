8d5fee4cd6b0d7a6a022d2f756c311a71146f1e7
Alex Hayton <alex.hayton@gmail.com>
2010-08-22 18:33:18 +0100

    Added database hooks

--- data.js
+++
@@ -1,8 +0,0 @@
-var objConfig;
-
-// Make sure to define the "configloaded" function!
-// We load the game config dynamically to prevent
-function getConfig(strConfigFile, funcCallback)
-{
-	$.getJSON(strConfigFile, funcCallback);
-}
--- script/config.js
+++ script/config.js
@@ -1,4 +1,15 @@
-function getConfig(strConfigFile)
+var objConfig;
+
+// Make sure to define the "configloaded" function!
+// We load the game config dynamically to prevent
+function GetConfig(strConfigFile, funcCallback)
 {
-	
+	// Define an internal function to handle the config 
+	function ProcessConfig(objData, strStatus)
+	{
+		// Save the data away to the global config object.	
+		objConfig = objData;
+		funcCallback();
+	}
+	$.getJSON(strConfigFile, ProcessConfig);
 }
