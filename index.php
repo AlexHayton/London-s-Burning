1c0b2fb48c63d551fa9d3598bae9c3972b221ceb
Alex Hayton <alex.hayton@gmail.com>
2010-08-26 21:48:37 +0100

    Added project file. Added new divs

--- index.php
+++ index.php
@@ -4,9 +4,15 @@
 ?>
 <BODY>
 	<!-- Menu bar -->
-	<div style="height: 50px; width: 100%; background-color: yellow;"><h1>Test map page</h1></div>
+	<div id=menuframe style="margin: 0px; height: 50px; width: 100%; background-color: yellow; border-style: solid; border-width: 5px; border-color: green;"><h1>Test map page</h1></div>
+	
+	<!-- Selection Div -->
+	<DIV id=selectionframe style="float: left; height: 60%; width: 100px; border-style: dotted; border-width: 1px; ">&nbsp;</DIV>
+
+	<!-- Mini view div -->
+	<DIV id=miniframe style="float: left; height:40%; width: 100px;">&nbsp;</DIV>
 
 	<!-- Map view -->
-	<DIV id=mapframe style="height: 90%; width:80%; border-style=dashed;">&nbsp;</DIV>
+	<DIV id=mapframe style="position: relative; height: 90%; width: 100%; border-style: dashed; border-width: 1px;">&nbsp;</DIV>
 </BODY>
 </HTML>
